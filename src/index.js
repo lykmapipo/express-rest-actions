ddad16b0947a28a01ed34e762fbd9d8d2c0e0754
lykmapipo <lallyelias87@gmail.com>
2019-05-05 16:21:25 +0300

    feat(get-by-id-for): implement initial handler

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { app, Router } from '@lykmapipo/express-common';
 /**
  * @function getFor
  * @name getFor
- * @description Create http get handle for given options
+ * @description Create http get handler for given service options
  * @param {Object} optns valid getFor options
  * @param {Function} optns.get valid service function to invoke when get
  * @return {Function} valid express middleware to handle request
@@ -28,7 +28,7 @@ export const getFor = optns => {
   const options = mergeObjects(optns);
   const { get } = options;
 
-  // create http handlers
+  // create http handler to get resources
   const getAll = (request, response, next) => {
     // ensure service get
     if (!isFunction(get)) {
@@ -54,6 +54,60 @@ export const getFor = optns => {
 
   // return get handler
   return getAll;
+};
+
+/**
+ * @function getByIdFor
+ * @name getByIdFor
+ * @description Create http getById handler for given service options
+ * @param {Object} optns valid getByIdFor options
+ * @param {Function} optns.getById valid service function to invoke when getById
+ * @return {Function} valid express middleware to handle request
+ * @author lally elias <lallyelias87@mail.com>
+ * @license MIT
+ * @since 0.1.0
+ * @version 0.1.0
+ * @static
+ * @public
+ * @example
+ *
+ * const { app, getByIdFor } = require('@lykmapipo/express-rest-actions');
+ *
+ * const getById = (query, done) => done(null, { ... });
+ * app.get('/v1/users/:id', getByIdFor({ getById }));
+ *
+ */
+export const getByIdFor = optns => {
+  // ensure options
+  const options = mergeObjects(optns);
+  const { getById } = options;
+
+  // create http handler to get single resource
+  const getOne = (request, response, next) => {
+    // ensure service getById
+    if (!isFunction(getById)) {
+      return response.methodNotAllowed();
+    }
+
+    // obtain mquery options and id path params
+    const query = mergeObjects(request.mquery, { _id: request.params.id });
+
+    // handle request
+    const afterGetOne = (error, results) => {
+      // handle error
+      if (error) {
+        return next(error);
+      }
+      // handle success
+      return response.ok(results);
+    };
+
+    // invoke service getById
+    return getById(query, afterGetOne);
+  };
+
+  // return get handler
+  return getOne;
 };
 
 export { app, Router };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,5 @@
 import { clear, expect, testGet } from '@lykmapipo/express-test-helpers';
-import { app, getFor } from '../src/index';
+import { app, getFor, getByIdFor } from '../src/index';
 
 describe('getFor', () => {
   beforeEach(() => clear());
@@ -20,5 +20,27 @@ describe('getFor', () => {
   it('should handle http GET /resource with no service', done => {
     app.get('/v1/users', getFor());
     testGet('/v1/users').expect(405, done);
+  });
+});
+
+describe('getByIdFor', () => {
+  beforeEach(() => clear());
+
+  it('should handle http GET /resource/:id with provided service', done => {
+    const results = {};
+    const getById = (query, cb) => cb(null, results);
+    app.get('/v1/users/:id', getByIdFor({ getById }));
+    testGet('/v1/users/1')
+      .expect(200)
+      .end((error, { body }) => {
+        expect(error).to.not.exist;
+        expect(body).to.be.eql(results);
+        done(error, body);
+      });
+  });
+
+  it('should handle http GET /resource/:id with no service', done => {
+    app.get('/v1/users/:id', getByIdFor());
+    testGet('/v1/users/1').expect(405, done);
   });
 });
