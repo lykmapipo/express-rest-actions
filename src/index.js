988f676538aa3356a10330726731118bfc3843e4
lykmapipo <lallyelias87@gmail.com>
2019-05-05 16:11:51 +0300

    feat(get-for): implement initial handler

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
+import { isFunction } from 'lodash';
+import { mergeObjects } from '@lykmapipo/common';
 import { app, Router } from '@lykmapipo/express-common';
+
+/**
+ * @function getFor
+ * @name getFor
+ * @description Create http get handle for given options
+ * @param {Object} optns valid getFor options
+ * @param {Function} optns.get valid service function to invoke when get
+ * @return {Function} valid express middleware to handle request
+ * @author lally elias <lallyelias87@mail.com>
+ * @license MIT
+ * @since 0.1.0
+ * @version 0.1.0
+ * @static
+ * @public
+ * @example
+ *
+ * const { app, getFor } = require('@lykmapipo/express-rest-actions');
+ *
+ * const get = (query, done) => done(null, { data:[ ... ] });
+ * app.get('/v1/users', getFor({ get }));
+ *
+ */
+export const getFor = optns => {
+  // ensure options
+  const options = mergeObjects(optns);
+  const { get } = options;
+
+  // create http handlers
+  const getAll = (request, response, next) => {
+    // ensure service get
+    if (!isFunction(get)) {
+      return response.methodNotAllowed();
+    }
+
+    // obtain mquery options
+    const query = mergeObjects(request.mquery);
+
+    // handle request
+    const afterGetAll = (error, results) => {
+      // handle error
+      if (error) {
+        return next(error);
+      }
+      // handle success
+      return response.ok(results);
+    };
+
+    // invoke service get
+    return get(query, afterGetAll);
+  };
+
+  // return get handler
+  return getAll;
+};
 
 export { app, Router };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,24 @@
-import { clear } from '@lykmapipo/express-test-helpers';
-// import { getFor } from '../src/index';
+import { clear, expect, testGet } from '@lykmapipo/express-test-helpers';
+import { app, getFor } from '../src/index';
 
-describe('actions', () => {
+describe('getFor', () => {
   beforeEach(() => clear());
+
+  it('should handle http GET /resource with provided service', done => {
+    const results = { data: [] };
+    const get = (query, cb) => cb(null, results);
+    app.get('/v1/users', getFor({ get }));
+    testGet('/v1/users')
+      .expect(200)
+      .end((error, { body }) => {
+        expect(error).to.not.exist;
+        expect(body).to.be.eql(results);
+        done(error, body);
+      });
+  });
+
+  it('should handle http GET /resource with no service', done => {
+    app.get('/v1/users', getFor());
+    testGet('/v1/users').expect(405, done);
+  });
 });
